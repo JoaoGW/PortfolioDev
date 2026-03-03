7d110a6980344a3c25d09dc2c9ce1543c10f769d
JoaoPedro <cpsenha@gmail.com>
2026-03-02 21:33:37 -0300

    Adicionada a tecnologia Node.js a alguns projetos. Melhorias de formatação geral no código utilizando config do Prettier. O item Ribeiro no header agora é clicável. O botão de email no /sobre agora leva para a página de contato.

--- src/components/headerTop.tsx
+++ src/components/headerTop.tsx
@@ -1,21 +1,39 @@
-import { Github, Linkedin } from "lucide-react";
+import { useRouter } from "next/navigation";
+
 import { LanguageSelector } from "./languageSelector";
 
-export function HeaderTop(){
-  return(
+import { Github, Linkedin } from "lucide-react";
+
+export function HeaderTop() {
+  const route = useRouter();
+
+  return (
     <div className="fixed top-0 left-0 right-0 bg-[#171717f0] w-full h-auto p-5 flex flex-row justify-between z-50">
-      <div className="border border-white rounded-3xl py-2 px-5 w-28 flex flex-row justify-center">
+      <button
+        className="border border-white rounded-3xl py-2 px-5 w-28 flex flex-row justify-center"
+        onClick={() => {
+          route.push("/");
+        }}
+      >
         <span className="playwrite text-center">Ribeiro</span>
-      </div>
+      </button>
       <div className="flex flex-row items-center">
-        <a href='https://www.linkedin.com/in/jo%C3%A3o-pedro-do-carmo-ribeiro/' target='_blank' rel='noreferrer'>
-          <button className="mr-8" onClick={ () => {} }><Linkedin /></button>
+        <a
+          href="https://www.linkedin.com/in/jo%C3%A3o-pedro-do-carmo-ribeiro/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button className="mr-8" onClick={() => {}}>
+            <Linkedin />
+          </button>
         </a>
-        <a href='https://github.com/JoaoGW' target='_blank' rel='noreferrer'>
-          <button className="mr-8" onClick={ () => {} }><Github /></button>
+        <a href="https://github.com/JoaoGW" target="_blank" rel="noreferrer">
+          <button className="mr-8" onClick={() => {}}>
+            <Github />
+          </button>
         </a>
         <LanguageSelector />
       </div>
     </div>
-  )
+  );
 }
